d40c2a9fbfdf2c3af6d5b0fb381573567607477b
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-04-10 18:59:44 +0600

    constructed the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import dollarImg from "../../assets/dollar 1.png"
+import navLogo from "../../assets/logo.png"
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="navbar bg-base-100 shadow-sm">
+    <div className="container mx-auto" >
+      <div className="navbar bg-base-100">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className=" text-xl">
+            <img src={navLogo} alt="" />
+          </a>
         </div>
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
+        <div className="flex items-center gap-20">
+            <a href="#">Home</a>
+            <a href="#">Fixture</a>
+            <a href="#">Teams</a>
+            <a href="#">Schedules</a>
+          <button className="btn flex justify-between items-center gap-2 font-bold text-xl">
             0 coins
             <img src={dollarImg} alt="" />
           </button>
