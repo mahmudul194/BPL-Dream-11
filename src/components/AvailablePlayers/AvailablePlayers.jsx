44ea47d3fce47a78b291367c99da8748ad2ea99c
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-04-11 23:01:15 +0600

    removed the card components from there

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { FaUser } from "react-icons/fa";
-import { IoMdFlag } from "react-icons/io";
+import Card from "../ui/Card";
 
 const AvailablePlayers = ({ players }) => {
   return (
@@ -8,37 +7,7 @@ const AvailablePlayers = ({ players }) => {
       <div className="grid grid-cols-3 sm:grid-cols-2 lg:grid-cols-3 justify-items-center mt-20">
         {players.map((player) => {
           return (
-            <div>
-              <div className="card bg-base-100 w-full max-w-sm shadow-sm m-5">
-                <figure>
-                  <img src={player.playerImg} alt="Virat Kohli" />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">
-                    <FaUser />
-                    {player.playerName}
-                  </h2>
-
-                  <div className="flex justify-between gap-2 items-center">
-                    <div className="flex items-center gap-2">
-                      <IoMdFlag />
-                      <p>{player.playerCountry}</p>
-                    </div>
-                    <button className="btn">{player.playerType}</button>
-                  </div>
-                  <div className="divider"></div>
-                  <h2 className="font-bold">rating: {player.rating}</h2>
-                  <div className="flex justify-between gap-4 font-bold">
-                    <p>{player.battingStyle}</p>
-                    <p className="text-right">{player.bowlingStyle}</p>
-                  </div>
-                  <div className="card-actions justify-between">
-                    <p className="font-semibold">Price : ${player.price}</p>
-                    <button className="btn">Choose Player</button>
-                  </div>
-                </div>
-              </div>
-            </div>
+           <Card player={player} ></Card>
           );
         })}
       </div>
