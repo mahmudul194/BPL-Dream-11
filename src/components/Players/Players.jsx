16733bd3b7221e9772cbfeca711b3324c21545b6
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-04-12 00:07:54 +0600

    added button toggle for available and selected players

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,11 +1,19 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import AvailablePlayers from '../AvailablePlayers/AvailablePlayers';
 
 const Players = ({playersPromise}) => {
     const players = use(playersPromise)
+    const [isSelected, setSelected] = useState("available");
+
     return (
-        <div className='container mx-auto' >
-            <h1>players : {players.length}</h1>
+        <div className='container mx-auto my-15' >
+            <div className='flex justify-between gap-4 items-center mb-5'>
+            <h1 className='font-bold text-4xl' >Available Players</h1>
+            <div>
+                <button onClick={ () => setSelected('available')} className={`btn ${isSelected === 'available' ? 'bg-[#E7F929]' : 'btn'} rounded-r-none rounded-l-xl`} >Available</button>
+                <button onClick={()=> setSelected('selected')} className={`btn rounded-l-none ${isSelected === 'selected' ? 'bg-[#E7F929]' : 'btn'} rounded-r-xl`} >selected</button>
+            </div>
+            </div>
 
             <AvailablePlayers players={players} ></AvailablePlayers>
         </div>
