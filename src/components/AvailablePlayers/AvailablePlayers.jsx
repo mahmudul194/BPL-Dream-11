4e59eaf7cb7872258762aba4f51036ecb5a72fad
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-04-11 23:27:52 +0600

    styled for the smaller screens

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -4,7 +4,7 @@ import Card from "../ui/Card";
 const AvailablePlayers = ({ players }) => {
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-3 sm:grid-cols-2 lg:grid-cols-3 justify-items-center mt-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center mt-20">
         {players.map((player) => {
           return (
            <Card player={player} ></Card>
