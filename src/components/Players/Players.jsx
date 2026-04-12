17e3f7a954b570a91666f1810ca0cb4c8092a16f
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-04-12 20:21:12 +0600

    passed the coins and setCoins data to selected player as props

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -10,14 +10,14 @@ const Players = ({playersPromise, setCoin, coin}) => {
     return (
         <div className='container mx-auto my-15' >
             <div className='flex justify-between gap-4 items-center mb-5'>
-            { isSelected === 'available' ? <h1 className='font-bold text-4xl' >Available Players</h1> : <h1 className='font-bold text-4xl' >Selected Players</h1> }
+            { isSelected === 'available' ? <h1 className='font-bold text-4xl' >Available Players</h1> : <h1 className='font-bold text-4xl' >Selected Players {selectedPlayers.length}/{players.length}</h1>}
             <div>
                 <button onClick={ () => setSelected('available')} className={`btn ${isSelected === 'available' ? 'bg-[#E7F929]' : 'btn'} rounded-r-none rounded-l-xl`} >Available</button>
-                <button onClick={()=> setSelected('selected')} className={`btn rounded-l-none ${isSelected === 'selected' ? 'bg-[#E7F929]' : 'btn'} rounded-r-xl`} >selected(0)</button>
+                <button onClick={()=> setSelected('selected')} className={`btn rounded-l-none ${isSelected === 'selected' ? 'bg-[#E7F929]' : 'btn'} rounded-r-xl`} >selected({selectedPlayers.length})</button>
             </div>
             </div>
 
-            { isSelected === 'available' ? <AvailablePlayers selectedPlayers={selectedPlayers} setSelectedPlayers={setSelectedPlayers} setCoin={setCoin} players={players} coin={coin}/> : <SelectedPlayers selectedPlayers={selectedPlayers} />}
+            { isSelected === 'available' ? <AvailablePlayers selectedPlayers={selectedPlayers} setSelectedPlayers={setSelectedPlayers} setCoin={setCoin} players={players} coin={coin}/> : <SelectedPlayers setSelectedPlayers={setSelectedPlayers} selectedPlayers={selectedPlayers} coin={coin} setCoin={setCoin} />}
         </div>
     );
 };
