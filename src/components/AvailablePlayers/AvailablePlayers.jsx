0f18a8d536d3d57d5a3485aa4ab4bf654259f730
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-04-12 15:46:56 +0600

    passed the coin and setCoin

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Card from "../ui/Card";
 
-const AvailablePlayers = ({ players }) => {
+const AvailablePlayers = ({ players,setCoin, coin }) => {
   return (
     <div className="container mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center mt-20">
         {players.map((player) => {
           return (
-           <Card player={player} ></Card>
+           <Card player={player} setCoin={setCoin} coin={coin} ></Card>
           );
         })}
       </div>
