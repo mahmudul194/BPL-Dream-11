39e2e5c5e21c9fa9be4bdd65eaa8c095f635a423
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-04-12 20:24:26 +0600

    formated the files

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import Card from "../ui/Card";
 
-const AvailablePlayers = ({ players,setCoin, coin,selectedPlayers, setSelectedPlayers }) => {
+const AvailablePlayers = ({
+  players,
+  setCoin,
+  coin,
+  selectedPlayers,
+  setSelectedPlayers,
+}) => {
   return (
     <div className="container mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center mt-20">
         {players.map((player, index) => {
           return (
-           <Card key={index} player={player} setCoin={setCoin} coin={coin} selectedPlayers={selectedPlayers} setSelectedPlayers={setSelectedPlayers} ></Card>
-          )
+            <Card
+              key={index}
+              player={player}
+              setCoin={setCoin}
+              coin={coin}
+              selectedPlayers={selectedPlayers}
+              setSelectedPlayers={setSelectedPlayers}
+            ></Card>
+          );
         })}
       </div>
     </div>
