63b47f77f9fb767dec10dcf3881d57c7257f730d
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-04-12 19:14:12 +0600

    added the key prop and passed through 2 values from player as the parent created another state

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Card from "../ui/Card";
 
-const AvailablePlayers = ({ players,setCoin, coin }) => {
+const AvailablePlayers = ({ players,setCoin, coin,selectedPlayers, setSelectedPlayers }) => {
   return (
     <div className="container mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center mt-20">
-        {players.map((player) => {
+        {players.map((player, index) => {
           return (
-           <Card player={player} setCoin={setCoin} coin={coin} ></Card>
-          );
+           <Card key={index} player={player} setCoin={setCoin} coin={coin} selectedPlayers={selectedPlayers} setSelectedPlayers={setSelectedPlayers} ></Card>
+          )
         })}
       </div>
     </div>
