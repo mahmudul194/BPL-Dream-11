e7879eadfca8f2dc8ae6a9c19f5f4d397cbe2a9d
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-04-12 20:22:54 +0600

    moved the delete functionality and the selected player card to the selected player card file

--- src/components/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { toast } from 'react-toastify';
+import SelectedPlayerCard from '../ui/SelectedPlayerCard';
 
-const SelectedPlayers = ({selectedPlayers}) => {
+const SelectedPlayers = ({selectedPlayers, setSelectedPlayers, setCoin, coin}) => {
+    const deleteSelectedPlayer = (player) => {
+        const filteredPlayers = selectedPlayers.filter(selectedplayer => selectedplayer.playerName != player.playerName)
+        toast('player Deleted!')
+        setSelectedPlayers(filteredPlayers)
+        setCoin(coin + player.price)
+    }
     return (
-        <div>
-            Selected Players
+        <div className='space-y-5'>
+            {
+                selectedPlayers.length === 0 ? <div className='h-100 flex items-center justify-center flex-col gap-4'>
+                    <h2>No players selected yet</h2>
+                    <p>Go to available tab to select players</p>
+                </div> :
+                selectedPlayers.map((player,index) =>{
+                    return (
+                        <SelectedPlayerCard player={player} key={index} deleteSelectedPlayer={deleteSelectedPlayer} ></SelectedPlayerCard>
+                    )
+                })
+            }
         </div>
     );
 };
